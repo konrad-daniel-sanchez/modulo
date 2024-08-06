29c6b1669563bd0fc2a2d60894c1efd5060ed6a3
... <scamargomontoya@gmail.com>
2024-08-05 20:39:12 -0500

    Se crea el proyecto con Netbeans

---
+++ CalcularModulo/src/main/java/com/mycompany/calcularmodulo/CalcularModulo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calcularmodulo;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class CalcularModulo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
